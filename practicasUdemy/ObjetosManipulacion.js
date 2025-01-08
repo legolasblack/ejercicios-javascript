6b0b7b8d974b1cd39f51ab6d4ae35227100cf00d
legolasblack <legolas.black.ds@gmail.com>
2025-01-07 22:47:00 -0600

    seguimos agregando practicas

--- practicasUdemy/ObjetosManipulacion.js
+++ practicasUdemy/ObjetosManipulacion.js
@@ -6,6 +6,9 @@ const persona = {
     email: "legolas_black@gmail.com ",
 }
 console.log(typeof persona)
+
+
+
 /*Destructuring de objetos
 con esta tecnica nos permite vaciar variables del objeto de manera simple y en una sola linea
 */
@@ -15,7 +18,8 @@ console.log(nombre)
 console.log(posicion)
 console.log(email)
 
-/**/
+/*con esta tecnica podemos omitir el agregado de una variable que ya esta previamente declarada en algun sitio
+y de esta manera hacer un asiganacion de valor a una variable*/
 const personaClonSombras ={
     nombre: "Ismael",
     posicion,
@@ -23,3 +27,24 @@ const personaClonSombras ={
 }
 
 console.log(personaClonSombras)
+
+/*Ejemplo de destructuring de dos o mas objetos*/
+
+const cliente= {
+    nombre: "juan",
+    premium : true,
+    direccion : {
+        calle: "emilano zapata 2345"
+    }
+}
+const producto= {
+    nombre: "iphone 8 ",
+    existencia : true
+}
+
+const {nombre: nombreProducto, direccion : {calle}}= producto
+const {nombre: nombreCliente}= cliente
+
+console.log(nombreProducto)
+console.log(nombreCliente)
+console.log(calle)
